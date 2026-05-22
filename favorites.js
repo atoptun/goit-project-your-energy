aab38dedfd2a42829fde400581a45a90faeacacc
oshamoiev <wamoev@gmail.com>
2026-05-22 17:57:39 +0000

    Deploying to gh-pages from @ oshamoiev/goit-advancedjs-fp-03@ceb65544fe9ab6e412f7eb2e9812d24483d4d380 🚀

--- favorites.js
+++ favorites.js
@@ -1,2 +1,2 @@
-import{n as e,t}from"./assets/mobile-menu-CUAA1dbf.js";e(),t();
+import{i as e,n as t,t as n}from"./assets/mobile-menu-CdVdLKHj.js";e(),t(),n();
 //# sourceMappingURL=favorites.js.map
