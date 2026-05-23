669773e9d987c3945b49c852db06c7c26f7c0943
atoptun <45485156+atoptun@users.noreply.github.com>
2026-05-23 14:01:39 +0000

    Deploying to gh-pages from @ oshamoiev/goit-advancedjs-fp-03@6144c1faff410bdd483e5752692679bc07eff787 🚀

--- favorites.js
+++ favorites.js
@@ -1,2 +1,2 @@
-import{i as e,n as t,t as n}from"./assets/mobile-menu-CdVdLKHj.js";e(),t(),n();
+import{i as e,n as t,t as n}from"./assets/mobile-menu-Cs0xTYFQ.js";e(),t(),n();
 //# sourceMappingURL=favorites.js.map
